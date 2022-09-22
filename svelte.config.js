33af7d68e9d2b8d3add388afc57256ee2833797d
nook <0xnook@protonmail.com>
2022-09-22 00:17:07 -0700

    make building site fully static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
+	outDir: 'build',
 	kit: {
 		adapter: adapter()
 	}
